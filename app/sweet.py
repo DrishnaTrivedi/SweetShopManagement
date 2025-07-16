d885bdf3e3201361f3dfa5268660fea2b043487a
DrishnaTrivedi <drishna4505@gmail.com>
2025-07-16 15:31:15 +0530

    Updated sweet class

--- app/sweet.py
+++ app/sweet.py
@@ -1,6 +1,6 @@
 class Sweet:
-    def __init__(self, id, name, category, price, quantity):
-        self.id = id
+    def __init__(self, sweet_id, name, category, price, quantity):
+        self.sweet_id = sweet_id
         self.name = name
         self.category = category
         self.price = price

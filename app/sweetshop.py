6a59c3618ea312fb544c98c8249e2b4443d1722e
DrishnaTrivedi <drishna4505@gmail.com>
2025-07-16 15:58:30 +0530

    Implemented delete sweet feature

--- app/sweetshop.py
+++ app/sweetshop.py
@@ -16,11 +16,27 @@ class SweetShop:
             if sweet.sweet_id == sweet_id:
                 return sweet
         raise SweetNotFoundError("Sweet not found.")
+    
+
+    def get_sweet_by_name(self, name):
+        for sweet in self.sweets:
+            if sweet.name == name:
+                return sweet
+        raise SweetNotFoundError("Sweet not found.")
 
     
-    def delete_sweet(self, sweet_id):
+    def delete_sweet_by_id(self, sweet_id):
         for sweet in self.sweets:
             if sweet.sweet_id == sweet_id:
                 self.sweets.remove(sweet)
                 return
         raise SweetNotFoundError("Sweet not found.")
+    
+
+    def delete_sweet_by_name(self, name):
+        for sweet in self.sweets:
+            if sweet.name == name:
+                self.sweets.remove(sweet)
+                return
+        raise SweetNotFoundError("Sweet not found.")
+    

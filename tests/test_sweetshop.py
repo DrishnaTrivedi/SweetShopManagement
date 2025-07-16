715cb8661d68fdfb2f69f57e03a7e2a3d56c2234
DrishnaTrivedi <drishna4505@gmail.com>
2025-07-16 15:48:52 +0530

    Add test for deleting sweet

--- tests/test_sweetshop.py
+++ tests/test_sweetshop.py
@@ -1,6 +1,16 @@
 import pytest
 from app.sweet import Sweet
 from app.sweetshop import SweetShop
+from app.errors import SweetNotFoundError, OutOfStockError
+
+# dummy sweets added to shop
+@pytest.fixture
+def sample_shop():
+    shop = SweetShop()
+    shop.add_sweet(Sweet(1001, "Kaju Katli", "Nut-Based", 50, 20))
+    shop.add_sweet(Sweet(1002, "Gulab Jamun", "Milk-Based", 10, 50))
+    shop.add_sweet(Sweet(1003, "Gajar Halwa", "Vegetable-Based", 30, 15))
+    return shop
 
 
 def test_add_sweet():
@@ -9,3 +19,10 @@ def test_add_sweet():
     shop.add_sweet(sweet)
     assert len(shop.get_all_sweets()) == 1
     assert shop.get_all_sweets()[0].name == "Kaju Katli"
+
+
+
+def test_delete_sweet(sample_shop):
+    sample_shop.delete_sweet(1001)
+    with pytest.raises(SweetNotFoundError):
+        sample_shop.get_sweet_by_id(1001)
